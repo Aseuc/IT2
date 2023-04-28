1b4118128d425a6ce1e90d6c0fab8318a06cecc3
Aseuc <78257976+Aseuc@users.noreply.github.com>
2023-04-28 11:15:01 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,31 @@
-const redLight = document.querySelector('.red');
-const yellowLight = document.querySelector('.yellow');
-const greenLight = document.querySelector('.green');
-
+const redLight = document.getElementById("redlight");
+const yellowLight = document.getElementById('yellowlight');
+const greenLight = document.getElementById('greenlight');
+const whiteLight = document.getElementById('whitelight');
 let activeLight = redLight;
 
-setInterval(() => {
-  activeLight.classList.remove('on');
+
+
+
+ setInterval(() => {
 
   if (activeLight === redLight) {
-    activeLight = greenLight;
-  } else if (activeLight === yellowLight) {
-    activeLight = redLight;
-  } else {
+    redLight.style.opacity = 1;
+    whiteLight.style.opacity = 0.3;
     activeLight = yellowLight;
-  }
+  } else if (activeLight === yellowLight) {
+    yellowLight.style.opacity = 1;
+    redLight.style.opacity = 0.3;
+    activeLight = greenLight;
+  } else if (activeLight === greenLight) {
+    greenLight.style.opacity = 1;
+    yellowLight.style.opacity = 0.3;
+    activeLight = whiteLight;
+   } else if (activeLight === whiteLight) {
+        whiteLight.style.opacity = 1;
+        greenLight.style.opacity = 0.3;
+        activeLight = redLight;
 
-  activeLight.classList.add('on');
-}, 2000);
+    }
+  
+}, 1000);
