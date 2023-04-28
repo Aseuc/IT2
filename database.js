98e524cd8781bc5004d6a1be9ca95b0ade0d1d61
Aseuc <busse1997@googlemail.com>
2023-04-28 12:47:35 +0200

    update

--- database.js
+++ database.js
@@ -1,5 +1,3 @@
-const databaseURI = "http://217.83.108.206:5984/";
-
 async function requestDatabase(method, path, body = {}) {
   let response;
   if (method === "GET" || method === "HEAD") {
@@ -40,11 +38,6 @@ async function login(user, pass) {
   console.log(result);
   return result;
 }
-/* 
-async function login1() {
-  login("kit", "kat");
-  console.log(getUser())
-} */
 
 async function getUser() {
   const result = await requestDatabase("GET", "_session");
@@ -56,50 +49,3 @@ async function logout() {
   await requestDatabase("DELETE", "_session");
   location.href = "../../";
 }
-
-/* async function request() {
-  alert(requestDatabase("GET", "userdata//_all_docs?include_docs=true", ""));
-}
- */
-
-/* async function putUserData() {
-    getIDs = await requestDatabase(
-        "GET",
-        "userdata/_all_docs?include_docs=true"
-      );
-    array = getIDs.message.rows;
-    cursor = 0
-    if(array.length <= 0) {
-    cursor = 1111111111
-    }else{
-    
-    cursor = array[array.length - 1].doc.cursor;
-    cursor++
-    }
-
-    var json = {
-        _id: `${cursor}`,
-        cursor: `${cursor}`,
-        reputationSupervisor: `${"hi"}`,
-        lastnameSupervisor: `${"hi"}`,
-        firstnameSupervisor: `${"hi"}`,
-        supervisorEmail: `${"hi"}` + "@fau.de",
-        departmentSupervisor: `${"jo"}`,
-        permissionSupervisor: `${"k"}`,
-      };
-
-      await requestDatabase("PUT", "userdata/" + cursor, json);
-  } */
-/* 
-  async function getUserData(){
-    getIDs = await requestDatabase(
-        "GET",
-        "userdata/_all_docs?include_docs=true"
-      );
-
-console.log(getIDs.message.rows);
-      return getIDs;
-  }
-
-
-  getUserData(); */
