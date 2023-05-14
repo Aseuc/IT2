682292669a7ce7b989eb178d186cb6891111fdf8
Aseuc <78257976+Aseuc@users.noreply.github.com>
2023-05-14 21:28:48 +0000

    update

--- 3djs.js
+++ 3djs.js
@@ -31,14 +31,30 @@ for (let i = 0; i < data4.length; i++) {
 
   let timeArray = Xdata.map((str) => str.replace(/\d{2}\.\d{2}\.\d{4}/, ''));
 
+  timeArray = timeArray.map(function (value) {return value.slice(5);});
+  
   
   let timeArray2 = getMinutesEveryFive(timeArray);
 
-  
+  Ydata = Ydata.map(function(value){ return value.replace(",",".")})
   const options = {
+    dataLabels: {
+      enabled: true,
+      background: {
+        enabled: true,
+        foreColor: '#fff',
+        borderColor: '#000',
+        borderWidth: 1,
+        borderRadius: 2,
+        padding: 4,
+        opacity: 0.9
+      }
+    },
     toolbar: {
      borderColor:'black',
-        
+     tools: {
+      pan: true
+    }
       },
     
     chart: {
@@ -94,8 +110,8 @@ for (let i = 0; i < data4.length; i++) {
           {
             y: 150,
             y2:75,
-            borderColor: '#000',
-            fillColor: '#FEB019',
+            borderColor: 'red',
+            fillColor: 'red',
             label: {
               text: 'Schadensgrenze_temp: 150',
               position: 'left',
@@ -104,20 +120,22 @@ for (let i = 0; i < data4.length; i++) {
           },
           {
             y: 75,
-            borderColor: '#000',
-            fillColor: '#FEB019',
-            
+            borderColor: 'red',
+            fillColor: 'red',
+ 
             label: {
               text: 'Toleranzgrenze_temp: 75',
               position: 'left',
-              offsetX: 130,
+              offsetX: 127,
+            
               
-              
-            }
+            },
+     
           }
         ]
       },
       yaxis: {
+        
         min: 0,
         max: 300,
         title:{
@@ -126,10 +144,11 @@ for (let i = 0; i < data4.length; i++) {
             fontSize: '15px'
           },
         },
-      },
+      }, 
       xaxis: {
-        labels: {},
+      
         categories: timeArray2,
+        
       }
     };
 
@@ -174,10 +193,27 @@ for (let i = 0; i < data4.length; i++) {
     
     
     let timeArray2 = getMinutesEveryFive(timeArray);
-  
+    Ydata = Ydata.map(function(value){ return value.replace(",",".")})
     
     const options = {
-    
+      dataLabels: {
+        enabled: true,
+        background: {
+          enabled: true,
+          foreColor: '#fff',
+          borderColor: '#000',
+          borderWidth: 1,
+          borderRadius: 2,
+          padding: 4,
+          opacity: 0.9
+        }
+      },
+      toolbar: {
+        borderColor:'black',
+        tools: {
+         pan: true
+       }
+         },
       chart: {
         fontFamily:'Roboto, sans-serif',
         type: 'line',
@@ -204,10 +240,10 @@ for (let i = 0; i < data4.length; i++) {
             formatter: function (value, { series, seriesIndex, dataPointIndex, w }) {
               if (value >= 117) {
                 
-                return 'Schadensgrenze überschritten!: ' + value;
+                return 'Schadensgrenze_laut überschritten!: ' + value;
               } else if (value >= 80) {
                 
-                return 'Toleranzgrenze überschritten!: ' + value;
+                return 'Toleranzgrenze_laut überschritten!: ' + value;
               } else {
                 return "Kugellagerakustik Normal: " + value;
               }
@@ -228,19 +264,19 @@ for (let i = 0; i < data4.length; i++) {
             {
               y: 117,
               y2:80,
-              borderColor: '#000',
-              fillColor: '#FEB019',
+              borderColor: 'red',
+              fillColor: 'red',
               label: {
-                text: 'Schadengrenze_laut: 117',
+                text: 'Schadensgrenze_laut: 117',
                 position: 'left',
-                offsetX:129,
+                offsetX:134,
               }
             },
             {
               y: 80,
-              borderColor: '#000',
-              fillColor: '#FEB019',
-              
+              borderColor: 'red',
+              fillColor: 'red',
+              backgroundColor:"green",
               label: {
                 text: 'Toleranzgrenze_laut: 80',
                 position: 'left',
@@ -262,8 +298,7 @@ for (let i = 0; i < data4.length; i++) {
           },
         },
         xaxis: {
-          labels: {},
-          categories: timeArray2,
+          categories: timeArray,
         }
       };
   
@@ -275,160 +310,167 @@ for (let i = 0; i < data4.length; i++) {
     
     }
 
-async function visualizeData3(){
-  const response = await fetch('https://it2wi1.if-lab.de/rest/mpr_fall3');
-  const data = await response.json();
-  console.log(data)
-  
-  let Ydata = [];
-  let Xdata = [];
-  for (let i = 0; i < data.length; i++) {
-     Ydata.push(data[i].werte.Tavg_temp);
-     Xdata.push(data[i].datum);
-  }
-  
-  
-  let timeArray = Xdata.map((str) => str.replace(/\d{2}\.\d{2}\.\d{4}/, ''));
-      timeArray = getMinutesEveryFive(timeArray);
-  
-  const options = {
-      chart: {
-        type: 'line',
-        width: 500,
-        height: 500,
-        background: '#859cac'
-      },  
-  
-      title: {
-        text: 'Mindere Qualität',
-        align: 'center',
-        margin: 10,
-        offsetX: 0,
-        offsetY: 0,
-        floating: false,
-        style: {
-          fontSize: '18px',
-          fontWeight: 'bold',
-          color: '#263238'
-        }
-      },
-      tooltip: {
-        theme: "dark",
-        y: {
-          formatter: function (value, { series, seriesIndex, dataPointIndex, w }) {
-            if (value >= 75) {
-              return 'Toleranzgrenze überschritten!: ' + value;
-            } else if (value >= 150){
-              return 'Schadensgrenze überschritten!: ' + value;
-            }
-          }
-        },
-        style: {
-          backgroundColor: "FF0099"
-        }
-      },
-      series: [
-        {
-          name: '',
-          data: Ydata,
-        }
-      ],
-      xaxis: {      
-          labels: {
-              rotate: -40,
-              offsetY: 10 // Fügt einen Abstand von 10 Pixeln zwischen der x-Achse und dem Label hinzu
-              
-          },
-      
-        categories: timeArray,
-      },
-      annotations: {
-          xaxis: []
-        }
-    };
-  const chart = new ApexCharts(document.querySelector('#chart'), options);
-  chart.render();
-  }
-async function visualizeData4(){
-      const response = await fetch('https://it2wi1.if-lab.de/rest/mpr_fall4');
-      const data = await response.json();
-      
-      
+    async function visualizeData3(){
+      const response1= await fetch('https://it2wi1.if-lab.de/rest/mpr_fall1');
+      const response2 = await fetch('https://it2wi1.if-lab.de/rest/mpr_fall2');
+      const response3 = await fetch('https://it2wi1.if-lab.de/rest/mpr_fall3');
+      const response4 = await fetch('https://it2wi1.if-lab.de/rest/mpr_fall4');
+    
+      const data = await response1.json();
+      const data2 = await response2.json();
+      const data3 = await response3.json();
+      const data4 = await response4.json();
       let Ydata = [];
+      let Ydata2 = [];
       let Xdata = [];
       for (let i = 0; i < data.length; i++) {
-         Ydata.push(data[i]);
+         Ydata.push(data[i].werte.Tavg_vibr);
          Xdata.push(data[i].datum);
       }
+      for (let i = 0; i < data2.length; i++) {
+        Ydata.push(data2[i].werte.Tavg_vibr);
+        Xdata.push(data2[i].datum);
+     }
+     for (let i = 0; i < data3.length; i++) {
+      Ydata.push(data3[i].werte.Tavg_vibr);
+      Xdata.push(data3[i].datum);
+    }
+    for (let i = 0; i < data4.length; i++) {
+      Ydata.push(data4[i].werte.Tavg_vibr);
+      Xdata.push(data4[i].datum);
+    }
 
-      
       let timeArray = Xdata.map((str) => str.replace(/\d{2}\.\d{2}\.\d{4}/, ''));
       
       
       let timeArray2 = getMinutesEveryFive(timeArray);
   
-      
+      Ydata = Ydata.map(function(value){ return value.replace(",",".")})
+
       const options = {
-          chart: {
-            type: 'line',
-            width: 500,
-            height: 500,
+        dataLabels: {
+          enabled: true,
+          background: {
+            enabled: true,
+            foreColor: '#fff',
+            borderColor: '#000',
+            borderWidth: 1,
+            borderRadius: 3,
+            padding: 4,
+            opacity: 0.9
+          }
+        },
+        toolbar: {
+          tools: {
+           pan: true
+         }
+           },
+        chart: {
+          fontFamily:'Roboto, sans-serif',
+          type: 'line',
+            width: 600,
+            height: 600,
             background: '#859cac'
           },
           title: {
-            text: 'Fall4',
+            text: 'Anlageninnenleben – Vibration und Akustik ',
             align: 'center',
             margin: 10,
-            offsetX: 0,
-            offsetY: 0,
+        
             floating: false,
             style: {
-              fontSize: '18px',
-              fontWeight: 'bold',
+              fontSize: '15px',
+         
               color: '#263238'
             }
           },
           tooltip: {
             theme: "dark",
+            y: {
+              formatter: function (value, { series, seriesIndex, dataPointIndex, w }) {
+                if (value.toFixed(4) >= 0.4000) {
+               
+                  return 'Schadengrenze_vibr überschritten!: ' + value.toFixed(4);
+                } else if (value.toFixed(4) >= 0.1500) {
+     
+                  return 'Toleranzgrenze_vibr überschritten!: ' + value.toFixed(4);
+                } else {
+                  return "Anlageninnenleben Normal: " + value.toFixed(4);
+                }
+              }
+            },
             style: {
               backgroundColor: "FF0099"
             }
           },
           series: [
             {
-              name: 'Fall 4',
-              data: Ydata,
-            }
+              name: "",
+              data: Ydata
+            },
           ],
+          annotations: {
+            yaxis: [
+              {
+                y: 0.4000,
+                y2:0.1500,
+                borderColor: 'red',
+                fillColor: 'red',
+                label: {
+                  text: 'Schadensgrenze_vibr: 0.40000',
+                  position: 'left',
+                  offsetX:155,
+                }
+              },
+              {
+                y: 0.15000,
+                borderColor: 'red',
+                fillColor: 'red',
+                
+                label: {
+                  text: 'Toleranzgrenze_laut: 0.15000',
+                  position: 'left',
+                  offsetX: 150,
+                  
+                  
+                }
+              }
+            ]
+          },
+          yaxis: {
+            min: 0.0000,
+            max: 1.0000,
+            title: {
+              text: "Tavg_vibr",
+              style:{
+                fontSize: "15px",
+              },
+            },
+          },
           xaxis: {
+            title:{
+              text:"Zeit in Sekunden"
+            },
             labels: {},
             categories: timeArray2,
           }
         };
-      /* 
-      for (let i = 0; i < data.length; i++) {
-          console.log(data[i])
-      }
-      
-       */
-      
-      
-      
-      
-      
-      const chart = new ApexCharts(document.querySelector('#chart4'), options);
+    
+      const chart = new ApexCharts(document.querySelector('#chart3'), options);
       chart.render();
       
       
       
       
       }
+
 function getSecondsEveryFive(array) {
     const result = [];
     array.forEach(time => {
-      const minutes = parseInt(time.split(':')[2], 10);
+      const seconds = parseInt(time.split(':')[2], 10);
         
-      if (minutes % 5 === 0) {
+      if (seconds % 5 === 0) {
+        console.log(result)
         result.push(time);
       }
     });
@@ -449,5 +491,4 @@ function getMinutesEveryFive(array) {
   }
       visualizeData1()
       visualizeData2()
-/*       visualizeData3()
-      visualizeData4() */
+      visualizeData3()
